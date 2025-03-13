c5eb18408e13d8b75ac10a0bf05e4df24e7191ac
Gabriella Davoudpour <gdavoudpour@gmail.com>
2025-03-12 19:28:00 -0700

    Working Home and Options Page

---
+++ src/loginComponents.tsx
@@ -0,0 +1,17 @@
+// import React from "react"
+// import { useState } from "react";
+// import { useNavigate } from "react-router";
+
+// interface LoginState {
+//     isLoggedIn: boolean;
+// }
+function Login() {
+    // const navigate = useNavigate();
+    // const [isLoggedIn, setIsLoggedIn] = useState<LoginState>(isLoggedIn:false);
+    return (
+        <h1>Hello!</h1>
+    )
+// oauth
+// on login, should redirect user using react router to /options
+}
+export default Login;
---
+++ src/optionsComponents.tsx
@@ -0,0 +1,19 @@
+// components to be rendered to /options after user is logged in
+import { useState } from "react";
+// import { useNavigate } from "react-router";
+
+
+// Answer Questions box (with behavioral/technical button)
+function AnswerQuestions() {
+    return (
+        <div>
+        <button>Behavioral</button>
+        <button>Technical</button>
+        </div>
+    )
+}
+// Practice Answered questions box (with behavioral/technical button)
+
+
+export default AnswerQuestions;
+
